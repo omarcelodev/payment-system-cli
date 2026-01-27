66962d9851501b43e6549f43334b85c878adaa80
Marcelo Gomes <marcelogomesfilho3@gmail.com>
2026-01-27 19:03:30 -0300

    Add payment methods import and input validation functions
    Created payments/__init__.py to expose Pix, Card, and Boleto. Added input validation functions in untils/input.py to ensure correct user input for payment options and values. Updated upgrades.txt with refined project structure and reordered planned improvements.

---
+++ payments/__init__.py
@@ -0,0 +1,2 @@
+from .methods import Pix, Card, Boleto
+
---
+++ untils/input.py
@@ -0,0 +1,22 @@
+#Função garante que o usuário digite um número // O programa não quebra
+def get_option():
+    try:
+        chose = int(input("Escolha a forma de pagamento: "))
+    except ValueError:
+        print("Opção inválida")
+        return None
+    return chose
+
+
+
+#Função para garantir que o usuário não digite strings ou valores negativos/zero
+def get_value():
+    try:  # Garante que o valor seja um número flaot postivo e não quebre o programa
+        value = float(input("Valor da compra: R$"))
+        if value <= 0.0:
+            raise ValueError
+        return value # Retorna o valor correto
+    
+    except ValueError:
+        print("Valor Inválido")
+        return None
